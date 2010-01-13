74461a14edc90e729c575f1f1a1fbfd6cf736369
Dave Vasilevsky <djvasi@gmail.com>
2010-01-12 21:26:43 -0500

    reorganize files

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-#base=lmnopuz
-#file=lmnopuz/CheckPUZ.app/Contents/Resources/script
+base=lmnopuz
+file=lmnopuz/CheckPUZ.app/Contents/Resources/script
 
 #base=nicotine
 #file=nicotine/museek+-0.1.13/doc/SConscript
 
-base=simbl
-file=Users/vasi/Desktop/SIMBL/keywurl/SIMBL.pkg/Contents/Info.plist
+#base=simbl
+#file=Users/vasi/Desktop/SIMBL/keywurl/SIMBL.pkg/Contents/Info.plist
 
 echo XZ
-time xz -c < $base.tar > $base.txz
-time xz -cd < $base.txz | tar xO $file | md5sum
+time xz -c < tars/$base.tar > tars/$base.txz
+time xz -cd < tars/$base.txz | tar xO $file | md5sum
 
 echo; echo; echo PIXZ
-time ./write $base.tar $base.tpxz
-time ./read $base.tpxz $file | tar xO $file | md5sum
+time ./write tars/$base.tar tars/$base.tpxz
+time ./read tars/$base.tpxz $file | tar xO $file | md5sum
 
 echo; echo; echo CROSS
-xz -cd < $base.tpxz | tar xO $file | md5sum
+xz -cd < tars/$base.tpxz | tar xO $file | md5sum
 
 echo; echo
-du -sh $base.tar $base.tpxz $base.txz
+du -sh tars/$base.tar tars/$base.tpxz tars/$base.txz
