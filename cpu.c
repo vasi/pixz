e648788c64de4438de1e765498d59b1c4ab82414
James Robinson <jlrobins@socialserve.com>
2011-10-14 15:31:01 -0400

    Actually scale down if non-default max_procs and value is fewer than machine has.

--- cpu.c
+++ cpu.c
@@ -10,5 +10,10 @@ size_t num_threads(size_t max_procs) {
 		/* Could just clamp to the max avail, but could also indicate */
 		/* user is confused. So err on side of refusing */
 		die("Max procs requested was greater than those available.");
+
+	/* non-default max_procs requested and machine has more? Scale down. */
+	if (max_procs > 0 && num_procs > max_procs)
+		num_procs = max_procs;
+
     return num_procs;
 }
