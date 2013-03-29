b9d029e20f61099372f0288778a9524e908ed02b
Tomek Czyż <tomekczyz@gmail.com>
2013-03-29 22:53:18 +0100

    add simple test for 'keep_input'

--- test.sh
+++ test.sh
@@ -17,3 +17,11 @@ xz -cd < test.tpxz | tar xO "$sample" | md5sum
 echo; echo
 du -sh "$tarball" test.tpxz test.txz
 rm test.tpxz test.txz
+
+echo; echo " * test: keep input"
+./pixz -k < "$sample" > test.xz
+[ -f "$sample" ] && echo " + compression: ok" || (echo " - compression: file removed"; exit 1)
+./pixz -dk < test.xz > test.original
+[ -f "$sample" ] && echo " + decompression: ok" || (echo " - decompression: file removed"; exit 1)
+rm -f test.xz test.original
+
