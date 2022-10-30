6a9443d955ce742112c6c05ed68fc8c3d5b6c3e6
Wessel Dankers <wsl@fruit.je>
2022-10-30 18:06:51 +0100

    Only use available CPUs
    Not all online CPUs may be available for the current process,
    especially when CPU affinity is involved. In such cases too many
    threads will be created, which will unnecessarily compete for CPU
    time.
    
    Use sched_getaffinity() (if available) to determine the correct
    number of threads to create.

--- src/cpu.c
+++ src/cpu.c
@@ -1,5 +1,28 @@
+#define _GNU_SOURCE
+
 #include <unistd.h>
+
+#include "config.h"
+
+#ifdef HAVE_SCHED_GETAFFINITY
+
+#include <sched.h>
+#include <stdio.h>
+
+size_t num_threads(void) {
+    cpu_set_t cpu_set;
+    CPU_ZERO(&cpu_set);
+
+    if (sched_getaffinity(0, sizeof cpu_set, &cpu_set) == -1)
+        return sysconf(_SC_NPROCESSORS_ONLN);
+    else
+        return CPU_COUNT(&cpu_set);
+}
+
+#else
 
 size_t num_threads(void) {
     return sysconf(_SC_NPROCESSORS_ONLN);
 }
+
+#endif
